2f03e676937b84bb209d3d75cca648b7e4f03daa
Samantha <sam.t.nguyenn@gmail.com>
2021-03-21 17:43:24 -0500

    [hygiene] Remove extra empty whitespace line

--- src/ContextualTime/OnPhrase.cs
+++ src/ContextualTime/OnPhrase.cs
@@ -3,7 +3,6 @@ using System;
 namespace ContextualTime {
 	public class OnPhrase : TimePhrase {
 		public OnPhrase( DayOfWeek currentDay, DayOfWeek newDay ) {
-			
 			Quantity = Math.Abs( (int)currentDay - (int)newDay );
 		}
 
