cf721d62c2d07fcad33572e13f29d31db2ea368d
Samantha <sam.t.nguyenn@gmail.com>
2021-03-27 01:29:00 -0500

    [demo] Add a calendar component with Spectre.Console
    Other demo changes to improve UX:
     - Change "Current date/time" to "Today"
     - Change "Modified date/time" to "Then"
     - Change "Tense" field to be phrased within a sentence

--- samples/ContextualTime.Demo/Program.cs
+++ samples/ContextualTime.Demo/Program.cs
@@ -1,22 +1,48 @@
 using System;
-using System.Globalization;
-using ContextualTime;
+using Spectre.Console;
 
-Parser parser = new();
-while ( true ) {
-	Console.Write( "Type in a time phrase: " );
-	string timePhraseString = Console.ReadLine();
-	try {
-		TimePhrase timePhrase = parser.Parse( timePhraseString );
-		Console.WriteLine( $"┣ Current date/time:\t{DateTime.Now}");
-		Console.WriteLine( $"┣ Modified date/time:\t{timePhrase.GetAsDateTime().ToString(CultureInfo.InvariantCulture)}" );
-		Console.WriteLine( $"┣ Tense: \t{timePhrase.Tense}\t| Preposition: \"{timePhrase.Preposition}\"" );
-		Console.WriteLine( $"┗ Quantity: \t{timePhrase.Quantity:+0;-#}\t| Unit of time: {timePhrase.UnitOfTime}" );
-	}
-	catch ( Exception e ) {
-		Console.WriteLine( "Time unable to be read." );
-		Console.WriteLine( e.Message );
-	}
+namespace ContextualTime.Demo {
+	public class Program {
+		public static void Main() {
+			Parser parser = new();
+			while ( true ) {
+				Console.Write( "Type in a time phrase: " );
+				string timePhraseString = Console.ReadLine();
+				try {
+					TimePhrase timePhrase = parser.Parse( timePhraseString );
+					DateTime timePhraseDt = timePhrase.GetAsDateTime();
+					GetDetails( timePhrase, timePhraseDt );
+					GetCalendar( timePhraseDt );
+				}
+				catch ( Exception e ) {
+					Console.WriteLine( "Time unable to be read." );
+					Console.WriteLine( e.Message );
+				}
 
-	Console.WriteLine();
+				Console.WriteLine();
+			}
+		}
+
+		private static void GetDetails( TimePhrase timePhrase, DateTime timePhraseDt ) {
+			Console.WriteLine( $"┣ Today: {DateTime.Now:f}" );
+			Console.WriteLine( $"┣ Then:  {timePhraseDt:f}" );
+			AnsiConsole.MarkupLine( $"┣ This date happens in the [bold]{timePhrase.Tense.ToString().ToLowerInvariant()}[/]." );
+			Console.WriteLine( $"┣ Quantity: \t{timePhrase.Quantity:+0;-#}" );
+			Console.WriteLine( $"┗ Unit of time: {timePhrase.UnitOfTime}" );
+			Console.WriteLine();
+		}
+
+		private static void GetCalendar( DateTime timePhraseDt ) {
+			var calendar = new Calendar( timePhraseDt.Year, timePhraseDt.Month );
+			calendar.AddCalendarEvent( timePhraseDt.Year, timePhraseDt.Month, timePhraseDt.Day );
+
+			calendar.HighlightStyle( Style.Parse( "turquoise2 bold" ) );
+			calendar.HeaderStyle( Style.Parse( "turquoise2 bold" ) );
+			calendar.Border = TableBorder.Rounded;
+			calendar.BorderStyle( Style.Parse( "turquoise2" ) );
+			calendar.Alignment = Justify.Center;
+
+			AnsiConsole.Render( calendar );
+		}
+	}
 }
